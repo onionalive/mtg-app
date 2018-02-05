aadd40d08d9c58e3582847d86e71d8277109f78a
Stephen <stephenritchie0@gmail.com>
2018-02-04 20:01:08 -0800

    Modularize button && update logic && add pagination

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, View, Button, FlatList, Image, Text, ScrollView, Picker } from 'react-native';
-import { Input } from './common';
+import { ActivityIndicator, View, FlatList, Image, Text, ScrollView, Picker } from 'react-native';
+import { Input, Button } from './common';
 
 class Container extends Component {
-	state = { searchQuery: '', cardResults: [], loading: false, page: 1, searchType: 'name' };
+	state = { searchQuery: '', cardResults: [], loading: false, page: 1, searchType: 'name', cardsPerPage: 10 };
 
-	searchCards() {
+	init() {
 		this.setState({ 
 			loading: true,
 			cardResults: [],
 			page: 1
 		 });
-		fetch(`http://api.magicthegathering.io/v1/cards?contains=imageUrl&${this.state.searchType}=${this.state.searchQuery}&pageSize=10&page=${this.state.page}&orderBy=name`)
+	}
+
+	searchCards() {
+		fetch(`http://api.magicthegathering.io/v1/cards?contains=imageUrl&${this.state.searchType}=${this.state.searchQuery}&pageSize=${this.state.cardsPerPage}&page=${this.state.page}&orderBy=name`)
 		.then((response) => response.json())
 		.then((responseJson) => {
-			this.setState({ 
-				cardResults: responseJson.cards,
+			const allResults = this.state.cardResults.concat(responseJson.cards);
+			this.setState({
+				cardResults: allResults,
 				loading: false
 			 });
 		})
@@ -25,25 +29,24 @@ class Container extends Component {
 		});
 	}
 
-	loadMore() {
+	newSearch() {
+		this.init();
+		this.searchCards();
+	}
+
+	loadNext() {
 		const newPage = this.state.page + 1;
-		this.setState({ 
-			page: newPage,
-			loading: true
-		 });
-		fetch(`http://api.magicthegathering.io/v1/cards?contains=imageUrl&${this.state.searchType}=${this.state.searchQuery}&pageSize=10&page=${this.state.page}&orderBy=name`)
-		.then((response) => response.json())
-		.then((responseJson) => {
-			const allResults = this.state.cardResults.concat(responseJson.cards);
-			this.setState({ 
-				cardResults: allResults,
-				loading: false
-			 });
-		})
-		.catch((error) => {
-			this.setState({ loading: false });
-			console.error(error);
-		});
+		this.setState({ page: newPage });
+		if (this.state.cardResults.length >= (this.state.page * this.state.cardsPerPage)) {
+			this.searchCards();
+		}
+	}
+
+	loadPrev() {
+		if (this.state.page > 1) {
+			const newPage = this.state.page - 1;
+			this.setState({ page: newPage });
+		}
 	}
 
 	renderCardsList() {
@@ -56,7 +59,10 @@ class Container extends Component {
 				</View>
 			);
 		} else if (this.state.cardResults.length > 0) {
-			return this.state.cardResults.map((card) => {
+			const start = (this.state.page - 1) * this.state.cardsPerPage;
+			const end = start + this.state.cardsPerPage;
+			const cardsToRender = this.state.cardResults.slice(start, end);
+			return cardsToRender.map((card) => {
 				return (
 					<View key={card.id} style={cardResult}>
 						<Image 
@@ -64,36 +70,49 @@ class Container extends Component {
 							style={{width: 110, height: 150, margin: 10}}
 						/>
 						<View style={cardDetailsStyle}>
-							<Text>{card.name}</Text>
-							<Text>{card.type} | {card.rarity}</Text>
-							<Text>{card.text}</Text>
+							<Text style={{ paddingBottom: 5 }}>{card.name}</Text>
+							<Text style={{ paddingBottom: 5 }}>{card.type} | {card.rarity}</Text>
+							<Text style={{ paddingBottom: 5 }}>{card.text}</Text>
 						</View>
 					</View>
 				)
 			})
-		} else {
-			<Text>No Results!</Text>
+		} else if (this.state.searchQuery != '' && this.state.cardResults == 0) {
+			return (
+				<Text>No Results!</Text>				
+			)
 		}
 	}
 
-	renderLoadMoreButton() {
-		const { buttonStyle } = styles;
-
-		if (this.state.cardResults.length > 0) {
+	renderPaginateButtons() {
+		if (this.state.cardResults.length > 0 && this.state.page == 1) {
 			return (
 				<Button
-					style={buttonStyle}
-					onPress={this.loadMore.bind(this)}
-					title="Load More"
-					accessibilityLabel="Load More"
+					onPress={this.loadNext.bind(this)}
+					children="Next"
 				/>
+			)
+		} 
+		
+		if (this.state.cardResults.length > 0 && this.state.page >= 1) {
+			return (
+				<View style={{ flex: 1, flexDirection: "row" }} >
+					<Button
+						onPress={this.loadPrev.bind(this)}
+						children="Prev"
+					/>
+					<Button
+						onPress={this.loadNext.bind(this)}
+						children="Next"
+					/>
+				</View>
 			)
 		}
 	}
 
 
 	render() {
-		const { containerViewStyle, inputStyle, buttonStyle, contentContainer, searchSubContent, pickerStyle } = styles;
+		const { containerViewStyle, inputStyle, contentContainer, searchSubContent, pickerStyle } = styles;
 		
 		return (
 			<View style={containerViewStyle}>
@@ -124,17 +143,15 @@ class Container extends Component {
 							<Picker.Item label="Toughness" value="toughness" />
 						</Picker>
 						<Button
-							style={buttonStyle}
-							onPress={this.searchCards.bind(this)}
-							title="Search"
-							accessibilityLabel="Search"
+							onPress={this.newSearch.bind(this)}
+							children="Search"
 						/>
 					</View>
 				</View>
 				<ScrollView contentContainerStyle={contentContainer} style={{width: "100%"}}>
 					{this.renderCardsList()}
+					{this.renderPaginateButtons()}
 				</ScrollView>
-				{this.renderLoadMoreButton()}
 			</View>
 		);
 	}
@@ -151,9 +168,6 @@ const styles = {
 		flex: 1,
 		width: '70%',
 	},
-	buttonStyle: {
-		flex: 1,
-	},
 	pickerStyle: {
 		width: 300,
 		height: 40,
@@ -163,14 +177,13 @@ const styles = {
 		flexDirection: "row",
 		height: 10,
 		alignItems: "flex-start",
-		// justifyContent: "center"
 	},
 	cardResult: {
 		padding: 10,
 		borderWidth: 1,
 		borderRadius: 5,
 		margin: 10,
-		height: 200,
+		height: 300,
 		flex: 1,
 		flexDirection: "row",
 		alignItems: "center",
